24bec957bd502be5a358fe5f86270aa1074a45d6
Victor <victoroctavio4@gmail.com>
2023-08-15 17:36:18 -0300

    Cronometro + Temporizador

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -6,29 +6,11 @@
                 type="text" 
                 class="input" 
                 placeholder="Qual tarefa você deseja iniciar?"
+                v-model="descricao"
                 >
             </div>
             <div class="column">
-                <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    <section>
-                    <strong>
-                        00:00:00
-                    </strong>
-                </section>
-                <button class="button">
-                    <span class="icon">
-                        <i class="fas fa-play"></i>
-                    </span>
-                    <span>play</span>
-                </button>
-                <button class="button">
-                    <span class="icon">
-                        <i class="fas fa-stop"></i>
-                    </span>
-                    <span>stop</span>
-                </button>
-                </div>
-                
+                <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
             </div>
         </div>
     </div>
@@ -37,8 +19,25 @@
 <script lang="ts">
 
 import { defineComponent } from 'vue'
+import Temporizador from './Temporizador.vue';
+
 export default defineComponent({
-    name: 'FormulárioItem'
+    name: 'FormulárioItem',
+    components: { 
+        Temporizador 
+    },
+    data() {
+        return{
+            descricao: ''
+        }
+    },
+    methods:{
+        finalizarTarefa(tempoDecorrido: number) : void{
+            console.log('tempo da tarefa', tempoDecorrido)
+            console.log('Descricao da tarefa: ', this.descricao)
+            this.descricao = ''
+        }
+    }
 })
 </script>
 
